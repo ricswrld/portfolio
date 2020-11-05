00919a0f0d10792a7c59e20ddd8e1f05e79e7a40
ricswrld <6446tito@gmail.com>
2020-11-05 15:46:29 +0000

    v2.2.4

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
+  const nav = document.querySelector ('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  //toggle nav
 
   burger.addEventListener('click',()=>{
     nav.classlist.toggle('nav-active');
   });
+  //animate links
+  navLinks.forEach((link, index) => {
+    console.log(index);
+  });
+
 }
 
 navSlide();
