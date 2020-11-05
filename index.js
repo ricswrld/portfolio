a9ff352fb399ef00d9a61594d25f1560d3d4cc6d
ricswrld <6446tito@gmail.com>
2020-11-05 16:16:33 +0000

    v2.2.5

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector ('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
-  //toggle nav
+
 
   burger.addEventListener('click',()=>{
+      //toggle nav
     nav.classlist.toggle('nav-active');
-  });
-  //animate links
-  navLinks.forEach((link, index) => {
-    console.log(index);
+
+      //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+          link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards` ${index / 7 + 1.5}s
+      }
+    });
   });
 
 }
