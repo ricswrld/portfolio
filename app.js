ca9cd7d5ea4df004dd162304ec2530b5ab91065e
ricswrld <6446tito@gmail.com>
2020-11-05 12:47:35 +0000

    v1.1

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
 
+  burger.addEventListener('click', () =>{
+    nav.classlist.toggle('nav-active');
+  });
 }
+
+navSlide();
